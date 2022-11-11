99f3a9fd344f6f21b1f10d9a44d7aba0a760c949
rapinchuk <rapinchuk1998@gmail.com>
2022-11-12 00:20:20 +0200

    add popup

---
+++ js/modal.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let images = document.querySelectorAll(".img_block")
+    let modal = document.querySelector(".modal")
+    let modalImg = document.querySelector(".modal_img")
+    let modalClose = document.querySelector(".modal_close")
+    images.forEach(el=>{
+       
+        el.addEventListener("click",modalHandler)
+    })
+    modalClose.addEventListener("click",()=>{
+        modal.style.display = "none";
+        document.body.style.overflow = "auto"
+    })
+    modal.addEventListener("click",(e)=>{
+        if(e.target.classList.contains("modal_img")) {
+            return;
+            // code...
+          }
+        modal.style.display = "none";
+        document.body.style.overflow = "auto"})
+    
+
+    function modalHandler(e){
+        modal.style.display = "block";
+        modalImg.setAttribute("src",e.target.getAttribute("image-data"))
+        e.target.getAttribute("image-data")
+        console.log( window.getComputedStyle(e.target).getPropertyValue("image-data").toString())
+        document.body.style.overflow = "hidden"
+    }
+  });
